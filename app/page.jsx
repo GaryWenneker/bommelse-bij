a322e5bd1e42637453aec3b387b286c3597b86b5
Gary Wenneker <gary.wenneker@iodigital.com>
2025-04-07 11:59:54 +0200

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-import Link from 'next/link';
+import BackgroundVideo from 'components/background-video/background-video.component';
 import { Card } from 'components/card';
-import { ContextAlert } from 'components/context-alert';
-import { Markdown } from 'components/markdown';
-import { RandomQuote } from 'components/random-quote';
+import Image from 'next/image';
 import { getNetlifyContext } from 'utils';
+
+// import { ContextAlert } from 'components/context-alert';
+
+// import Link from 'next/link';
+// import { Markdown } from 'components/markdown';
+// import { RandomQuote } from 'components/random-quote';
+
+// import styles from './page.module.scss';
 
 const contextExplainer = `
 The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
@@ -25,27 +31,51 @@ const ctx = getNetlifyContext();
 
 export default function Page() {
     return (
-        <div className="flex flex-col gap-12 sm:gap-16">
-            <section>
-                <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
-                <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
-                    Read the Docs
-                </Link>
-            </section>
-            {!!ctx && (
-                <section className="flex flex-col gap-4">
-                    <Markdown content={contextExplainer} />
-                    <RuntimeContextCard />
-                </section>
-            )}
-            <section className="flex flex-col gap-4">
-                <Markdown content={preDynamicContentExplainer} />
-                <RandomQuote />
-                <Markdown content={postDynamicContentExplainer} />
-            </section>
-        </div>
+        // <div className="flex flex-col gap-12 sm:gap-16">
+        //     <section>
+        //         <ContextAlert className="mb-6" />
+        //         <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
+        //         <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
+        //         <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
+        //             Read the Docs
+        //         </Link>
+        //     </section>
+        //     {!!ctx && (
+        //         <section className="flex flex-col gap-4">
+        //             <Markdown content={contextExplainer} />
+        //             <RuntimeContextCard />
+        //         </section>
+        //     )}
+        //     <section className="flex flex-col gap-4">
+        //         <Markdown content={preDynamicContentExplainer} />
+        //         <RandomQuote />
+        //         <Markdown content={postDynamicContentExplainer} />
+        //     </section>
+        // </div>
+        <>
+            <BackgroundVideo>
+                <div>
+                    <div className="wrapper">
+                        <div className="container">
+                            <div id="welcome">
+                            </div>
+                            <Image
+                                className='logo'
+                                src="/images/logo.png"
+                                alt="alt"
+                                width={246}
+                                height={246}
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                    maxWidth: '500px'
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </BackgroundVideo>
+        </>
     );
 }
 
